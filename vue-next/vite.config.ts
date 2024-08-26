0cc999dd628c7aaa2a3c8efd5fadd48e8cfe674a
mitkimi <h@otian.me>
2024-08-26 16:44:16 +0800

    update: publish

--- vue-next/vite.config.ts
+++ vue-next/vite.config.ts
@@ -5,6 +5,14 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: 'package/index.ts', // 入口文件路径
+      name: '@apron.design/vue-next', // 库的名称
+      fileName: (format) => `@apron.design/vue-next.${format}.js`,
+    },
+    outDir: 'lib', // 输出目录
+  },
   plugins: [
     vue(),
     vueJsx()
